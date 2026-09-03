eef8416f4c0212bc54a0a0bc3b6ba3071f7bd29f
rhemz_apostol <rhemzjhoncyruscapostol@gmail.com>
2026-09-03 08:33:27 +0800

    group activity

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import StudentCard from "./StudentCard.jsx";
 
 // Students sorted alphabetically by last name
 const students = [
-  { name: "Abache, Noreal", id: "2026-001", course: "BS Information Technology", year: "3rd Year" },
+  { name: "Abache, Noreal", id: "MCC2024-00104", course: "BS Information Technology", year: "3rd Year" },
   { name: "Apostol, Rhemz Jhon Cyrus C.", id: "2026-002", course: "BS Computer Science", year: "3rd Year" },
   { name: "Goot, Pholl Vincent", id: "2026-003", course: "BS Information Technology", year: "3rd Year" },
   { name: "Torres, John Francis", id: "2026-004", course: "BS Computer Science", year: "3rd Year" },
